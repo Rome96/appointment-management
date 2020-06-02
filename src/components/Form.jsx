193f5f1a0087210b3970afedfac202dab6a84da1
Turiano Romero <romero.guzman18@gmail.com>
2020-06-01 20:48:21 -0500

    validacion de formulario

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,79 +1,97 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Form = () => {
-
   const [quotes, setQuotes] = useState({
-    hora:'',
-    fecha: '',
-    mascota: '',
-    sintomas: '',
-    propietario: '',
-  })
+    hora: "",
+    fecha: "",
+    mascota: "",
+    sintomas: "",
+    propietario: "",
+  });
+
+  const [error, setError] = useState(false)
 
   const handleChange = (e) => {
     setQuotes({
       ...quotes,
-      [e.target.name] : e.target.value
-    })
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
 
-  const { hora, fecha, mascota, sintomas, propietario } = quotes
+  const { hora, fecha, mascota, sintomas, propietario } = quotes;
+
+  // send form
+  const submitQuotes = (e) => {
+
+    e.preventDefault();
+
+    //validate from
+    if (
+      hora.trim() === "" ||
+      fecha.trim() === "" ||
+      mascota.trim() === "" ||
+      sintomas.trim() === "" ||
+      propietario.trim() === ""
+    ) {
+      setError(true)
+      return;
+    }
+  };
 
   return (
     <React.Fragment>
       <h2>Crear Citas</h2>
-      <form>
+      { 
+        error && <p className='error-text'>Todos los campos son obligatorios. </p> 
+      }
+      <form onSubmit={submitQuotes}>
         <label>Nombre de la mascota</label>
         <input
           type="text"
-          name='mascota'
+          name="mascota"
           value={mascota}
           onChange={handleChange}
-          className='u-full-width'
-          placeholder='nombre de la mascota'
+          className="u-full-width"
+          placeholder="nombre de la mascota"
         />
         <label>Nombre del dueño</label>
         <input
           type="text"
-          name='propietario'
+          name="propietario"
           value={propietario}
           onChange={handleChange}
-          className='u-full-width'
-          placeholder='nombre propietario de la mascota'
+          className="u-full-width"
+          placeholder="nombre propietario de la mascota"
         />
         <label>Fecha</label>
         <input
           type="date"
-          name='fecha'
+          name="fecha"
           value={fecha}
           onChange={handleChange}
-          className='u-full-width'
+          className="u-full-width"
         />
         <label>Hora</label>
         <input
           type="time"
-          name='hora'
+          name="hora"
           value={hora}
           onChange={handleChange}
-          className='u-full-width'
+          className="u-full-width"
         />
         <label>Síntomas</label>
         <textarea
           name="sintomas"
           value={sintomas}
           onChange={handleChange}
-          className='u-full-width'
-        >
-        </textarea>
-        <button
-          type='submit'
-          className='u-full-width button-primary'
-        >
+          className="u-full-width"
+        ></textarea>
+        <button type="submit" className="u-full-width button-primary">
           Agregar Cita
         </button>
       </form>
     </React.Fragment>
   );
-}
- 
+};
+
 export default Form;
