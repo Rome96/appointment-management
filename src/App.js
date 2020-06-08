4c561fd80a7a9f3713153a2a6fa982d917b4a8de
Turiano Romero <romero.guzman18@gmail.com>
2020-06-08 12:21:10 -0500

    guardando datos en localStorage

--- src/App.js
+++ src/App.js
@@ -1,51 +1,54 @@
-import React, { useState } from 'react';
-import Form from './components/Form'
-import Quote from './components/Quote'
+import React, { useState, useEffect } from "react";
+import Form from "./components/Form";
+import Quote from "./components/Quote";
 
-
-const RenderQuote = (quotes, deleteQuotes) => (
-  quotes.map(quote => (
+const RenderQuote = (quotes, deleteQuotes) =>
+  quotes.map((quote) => (
     <Quote
       key={quote.id}
       quote={quote}
       deleteQuotes={deleteQuotes}
     />
-  ))
-)
+  ));
 
 function App() {
-  const [ quotes, setQuotes ] = useState([])
+  //quotes in localStorage 
+  let initialQuotes = JSON.parse(localStorage.getItem("quotes"));
+  
+  if (!initialQuotes) {
+    initialQuotes = [];
+  }
+
+  const [quotes, setQuotes] = useState(initialQuotes);
+
+  // useEffect para realizar ciertas operaciones cuando el state cambia
+  useEffect(() => {
+    initialQuotes
+      ? localStorage.setItem("quotes", JSON.stringify(quotes))
+      : localStorage.setItem("quotes", JSON.stringify([]));
+  }, [quotes]);
 
   const createQuotes = (quote) => {
-    setQuotes([
-      ...quotes,
-      quote
-    ])
-  }
+    setQuotes([...quotes, quote]);
+  };
 
   const deleteQuotes = (id) => {
-    const newQuotes = quotes.filter(quote => quote.id !== id )
-    setQuotes(newQuotes)
-    console.log('Citas:', newQuotes)
-  }
+    const newQuotes = quotes.filter((quote) => quote.id !== id);
+    setQuotes(newQuotes);
+    console.log("Citas:", newQuotes);
+  };
 
   return (
     <React.Fragment>
       <h2>Administrador de pacientes</h2>
-      <div className='container'>
-        <div className='row'>
-          <div className='one-half column'>
-            <Form
-              createQuotes={createQuotes}
-            />
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Form createQuotes={createQuotes} />
           </div>
-          <div className='one-half column'>
-            <h3>
-              {
-                quotes.length ? 'Administra tus citas' : 'No hay citas'
-              }
-            </h3>
-            { RenderQuote(quotes, deleteQuotes) }
+          <div className="one-half column">
+            <h3>{quotes.length ? "Administra tus citas" : "No hay citas"}</h3>
+            {RenderQuote(quotes, deleteQuotes)}
           </div>
         </div>
       </div>
